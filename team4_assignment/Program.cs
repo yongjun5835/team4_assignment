1e5f01643fa62576c064a23183d143dc4cb0e28f
HamYoungjoo <hamyoungjoo1004@gmail.com>
2023-08-28 16:05:11 +0900

    캐릭터 이름 입력
    Program.cs의 첫 화면을 EnteranceUi에서 캐릭터 이름 입력하는 InputName으로 변경하고, 캐릭터 이름은 직접 입력받아 userName으로 설정.

--- team4_assignment/Program.cs
+++ team4_assignment/Program.cs
@@ -1,11 +1,18 @@
 ﻿
+using team4_assignment;
+
 internal class Program
-{
+{ 
     static void Main(string[] args)
     {
         new GameManager();
         Entrance entrance = new Entrance();
 
-        entrance.EntranceUI();
+        Character character = new Character();
+
+        character.InputName();// 캐릭터 생성기능
+        string userName = character.UserName;
+
+
     }
 }
