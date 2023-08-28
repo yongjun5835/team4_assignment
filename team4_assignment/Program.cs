92d66b4d5a0a7642a0f03f5d16fa23575415fef6
Jo Yongjun <yongjun5835@gmail.com>
2023-08-28 11:08:22 +0900

    ewfew

---
+++ team4_assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace team4_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
