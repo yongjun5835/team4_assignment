190c59daef157f1e8b80e583f8c21a84b91a33e9
Jo Yongjun <yongjun5835@gmail.com>
2023-08-28 17:55:52 +0900

    hahahaha

--- team4_assignment/FightScene.cs
+++ team4_assignment/FightScene.cs
@@ -9,6 +9,7 @@ internal class FightScene
     List<Monster> monsters = new List<Monster>();
     Random rand = new Random();
     int stageLevel;
+    int[] monsterIndex = new int[5];
 
     public FightScene()
     {
@@ -28,7 +29,6 @@ internal class FightScene
 
     public void StartPhase()
     {
-        int[] monsterIndex = new int[5];
         for (int i = 0; i < 5; i++)
         {
             if (stageLevel == 1)
@@ -45,7 +45,7 @@ internal class FightScene
             }
         }
 
-        DrawDisplay("", "");
+        DrawDisplay("도망가기", "맞서 싸우기", "생선과 친해지기", "생선과 대화하기");
         ShowChoice();
     }
 
@@ -64,6 +64,16 @@ internal class FightScene
                 isSelect = true;
             }
             else if (key == "1")
+            {
+                PlayerPhase();
+                isSelect = true;
+            }
+            else if (key == "2")
+            {
+                PlayerPhase();
+                isSelect = true;
+            }
+            else if (key == "3")
             {
                 PlayerPhase();
                 isSelect = true;
@@ -93,7 +103,7 @@ internal class FightScene
 
     }
 
-    public void DrawDisplay(string choice1, string choice2)
+    public void DrawDisplay(string choice1, string choice2, string choice3, string choice4)
     {
         Console.Clear();
 
@@ -137,5 +147,162 @@ internal class FightScene
         Console.Write($"DEF: {Program.player.Def}");
         Console.SetCursorPosition(1, 12);
         Console.Write("-----------------------------------------------------------------------------------");
+
+        Console.ForegroundColor= ConsoleColor.Yellow;
+        Console.SetCursorPosition(2, 16);
+        Console.Write("생");
+        Console.SetCursorPosition(2, 18);
+        Console.Write("선");
+        Console.ResetColor();
+        for (int i = 13; i < 23; i++)
+        {
+            Console.SetCursorPosition(4, i);
+            Console.Write("|");
+        }
+
+        for (int i = 13; i < 23; i++)
+        {
+            Console.SetCursorPosition(20, i);
+            Console.Write("|");
+        }
+        if (stageLevel >= 1)
+        {
+            Console.SetCursorPosition(10, 15);
+            Console.Write($"{monsters[monsterIndex[0]].Name}");
+            Console.SetCursorPosition(10, 17);
+            Console.Write($"HP: {monsters[monsterIndex[0]].Hp}");
+            Console.SetCursorPosition(10, 18);
+            Console.Write($"ATK: {monsters[monsterIndex[0]].Atk}");
+            if (monsters[monsterIndex[0]].Hp > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(9, 20);
+                Console.Write($"[전투중]");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(9, 20);
+                Console.Write($"[잡았다!]");
+                Console.ResetColor();
+            }
+        }
+
+        for (int i = 13; i < 23; i++)
+        {
+            Console.SetCursorPosition(36, i);
+            Console.Write("|");
+        }
+        if (stageLevel >= 1)
+        {
+            Console.SetCursorPosition(26, 15);
+            Console.Write($"{monsters[monsterIndex[1]].Name}");
+            Console.SetCursorPosition(26, 17);
+            Console.Write($"HP: {monsters[monsterIndex[1]].Hp}");
+            Console.SetCursorPosition(26, 18);
+            Console.Write($"ATK: {monsters[monsterIndex[1]].Atk}");
+            if (monsters[monsterIndex[1]].Hp > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(25, 20);
+                Console.Write($"[전투중]");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(25, 20);
+                Console.Write($"[잡았다!]");
+                Console.ResetColor();
+            }
+        }
+
+        for (int i = 13; i < 23; i++)
+        {
+            Console.SetCursorPosition(52, i);
+            Console.Write("|");
+        }
+        if (stageLevel >= 1)
+        {
+            Console.SetCursorPosition(42, 15);
+            Console.Write($"{monsters[monsterIndex[2]].Name}");
+            Console.SetCursorPosition(42, 17);
+            Console.Write($"HP: {monsters[monsterIndex[2]].Hp}");
+            Console.SetCursorPosition(42, 18);
+            Console.Write($"ATK: {monsters[monsterIndex[2]].Atk}");
+            if (monsters[monsterIndex[2]].Hp > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(41, 20);
+                Console.Write($"[전투중]");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(51, 20);
+                Console.Write($"[잡았다!]");
+                Console.ResetColor();
+            }
+        }
+
+        for (int i = 13; i < 23; i++)
+        {
+            Console.SetCursorPosition(68, i);
+            Console.Write("|");
+        }
+        if (stageLevel >= 2)
+        {
+            Console.SetCursorPosition(58, 15);
+            Console.Write($"{monsters[monsterIndex[3]].Name}");
+            Console.SetCursorPosition(58, 17);
+            Console.Write($"HP: {monsters[monsterIndex[3]].Hp}");
+            Console.SetCursorPosition(58, 18);
+            Console.Write($"ATK: {monsters[monsterIndex[3]].Atk}");
+            if (monsters[monsterIndex[3]].Hp > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(57, 20);
+                Console.Write($"[전투중]");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(57, 20);
+                Console.Write($"[잡았다!]");
+                Console.ResetColor();
+            }
+        }
+
+        if (stageLevel >= 3)
+        {
+            Console.SetCursorPosition(74, 15);
+            Console.Write($"{monsters[monsterIndex[4]].Name}");
+            Console.SetCursorPosition(74, 17);
+            Console.Write($"HP: {monsters[monsterIndex[4]].Hp}");
+            Console.SetCursorPosition(74, 18);
+            Console.Write($"ATK: {monsters[monsterIndex[4]].Atk}");
+            if (monsters[monsterIndex[4]].Hp > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(73, 20);
+                Console.Write($"[전투중]");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(73, 20);
+                Console.Write($"[잡았다!]");
+                Console.ResetColor();
+            }
+        }
+
+        Console.SetCursorPosition(1, 23);
+        Console.Write("-----------------------------------------------------------------------------------");
+        Console.SetCursorPosition(2, 24);
+        Console.Write($"0. {choice1} | 1. {choice2} | 2. {choice3} | 3. {choice4} |");
     }
 }
