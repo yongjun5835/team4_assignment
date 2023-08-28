88cd8f8eaec7a4da5ec64dd93517c9b891a30749
lightlight98 <light0425@gmail.com>
2023-08-28 15:49:59 +0900

    CheckLevelUp

--- team4_assignment/Player.cs
+++ team4_assignment/Player.cs
@@ -1,4 +1,7 @@
-﻿class Player : Unit
+﻿using System.Reflection;
+using System.Windows.Markup;
+
+class Player : Unit
 {
     public Player()
     {
@@ -6,13 +9,20 @@
         atk = 10;
         def = 5;
         gold = 1500;
+        level = 1;
+        exp = 0;
+        
     }
 
     private string job = "";
     private int gold;
+    private int exp;
 
     public string Job { get { return job; } set { job = value; } }
     public int Gold { get { return gold; } set { gold = value; } }
+    public int Level { get; set; }
+    public int Exp { get; set; }
+
 
     public void StatusUI()
     {
@@ -30,6 +40,24 @@
                 return;
             default:
                 break;
+        }
+    }
+    public void CheckLevelup() //레벨업 체크
+    {
+        int[] maxExp = { 0, 10, 35, 65, 100 };//레벨별 필요 경험치
+
+        for (int i = level + 1; i < maxExp.Length; i++)
+        {
+            if (exp >= maxExp[i])
+            {
+                level++; //레벨업
+                atk += 0.5;
+                def++;
+            }
+            else
+            {
+                break;
+            }
         }
     }
 }
