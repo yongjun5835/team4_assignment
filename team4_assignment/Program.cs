5e9e165d1683c6ec2c3a5db3646d71cc3864b9bb
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2023-08-28 14:01:20 +0900

    기초작업

--- team4_assignment/Program.cs
+++ team4_assignment/Program.cs
@@ -1,13 +1,11 @@
-﻿namespace team4_assignment
+﻿
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Entrance entrance = new Entrance();
-            GameManager gameManager = new GameManager();
+        new GameManager();
+        Entrance entrance = new Entrance();
 
-            entrance.EntranceUI();
-        }
+        entrance.EntranceUI();
     }
 }
