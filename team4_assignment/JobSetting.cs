8ff59fd83c634647a022d70c5911fe52f0a27a40
HamYoungjoo <hamyoungjoo1004@gmail.com>
2023-08-29 15:03:16 +0900

    직업 정보 업뎃
    직업 정보 보이게 ( 배열로 다시 묶어서 정리해 볼 예정)

--- team4_assignment/JobSetting.cs
+++ team4_assignment/JobSetting.cs
@@ -14,7 +14,7 @@ namespace team4_assignment
         {
             Console.Clear();
             Console.WriteLine("원하시는 직업을 선택하세요.\n");
-            Console.WriteLine("1.궁수 2.전사 3.마법사 \n");
+            Console.WriteLine("1. 궁수(공격력 : 7 방어력 : 7) \n2. 전사(공격력 : 10 방어력 : 4)\n3. 마법사(공격력 : 5 방어력 : 9)");
 
             int optionNum = 3;
             int input = GameManager.GM.SelectOption(optionNum, false, "");
@@ -46,8 +46,8 @@ namespace team4_assignment
                     return;
             }
             Console.Clear();
-            Console.WriteLine($"당신의 직업은 {jobName}입니다.");
-            Console.WriteLine("1. 입장 하기 \n2. 직업 다시 고르기");
+            Console.WriteLine($"당신이 선택한 직업은 {jobName}입니다.\n");
+            Console.WriteLine("1. 확인 \n2. 다시 선택");
 
             Program.player.Job = jobName; // 직업 선택 시 player 상태보기에서 직업 이름이 입력됨.
             Program.player.Atk = jobAtk; 
