0ababe2042200858ccd26baa69e61b266817dcd1
Jo Yongjun <yongjun5835@gmail.com>
2023-08-29 15:10:07 +0900

    보스 구현

--- team4_assignment/Monster.cs
+++ team4_assignment/Monster.cs
@@ -8,10 +8,10 @@ internal class Monster : Unit
 {
     public Monster(int type)
     {
-        int[] monsterHp = { 10, 20, 30, 40, 50 };
-        int[] monsterAtk = { 5, 10, 15, 20, 25 };
-        int[] monsterExp = { 5, 10, 15, 20, 25 };
-        string[] monsterName = { "송사리", "꺽지", "블루길", "농어", "참치" };
+        int[] monsterHp = { 10, 20, 30, 40, 50, 500 };
+        int[] monsterAtk = { 5, 10, 15, 20, 25, 95 };
+        int[] monsterExp = { 5, 10, 15, 20, 25, 95 };
+        string[] monsterName = { "송사리", "꺽지", "블루길", "농어", "참치", "자이언트참치" };
 
         this.hp = monsterHp[type];
         this.atk = monsterAtk[type];
