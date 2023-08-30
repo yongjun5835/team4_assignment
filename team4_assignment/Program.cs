3b0f70243fd982c3fcc5776806b0a8a5b8751055
HamYoungjoo <hamyoungjoo1004@gmail.com>
2023-08-30 15:36:39 +0900

    잘못된 입력 이동 문제
    메인 메소드에서 인벤토리 부분 삭제 , character 클래스의 입력 부분을 whlie 문으로 변경. 단 아직 직업 선택부분에 input 값 이외의 값을 입력하면 새로운 입력을 받지 않고 실행이 멈추는 부분은 수정이 필요함.

--- team4_assignment/Program.cs
+++ team4_assignment/Program.cs
@@ -9,11 +9,6 @@ internal class Program
         Character character = new Character();
         character.InputName();// 캐릭터 생성기능
         string userName = character.UserName;
-
-        // Inventory 객체 생성
-        Inventory inventory = new Inventory();
-        inventory.DisplayInventory();
-        inventory.InventoryEquip();
     }
 
     public static FightScene fightScene = new FightScene();
