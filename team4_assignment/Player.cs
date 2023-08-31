4617fed4c8aa6e5d4e313299c81aae2aceb70922
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2023-08-31 20:00:14 +0900

    Add. 정보보기 추가
    UI 그래픽, 직업별 소개글

--- team4_assignment/Player.cs
+++ team4_assignment/Player.cs
@@ -1,4 +1,7 @@
-﻿class Player : Unit
+﻿using System.Runtime;
+using System.Text;
+
+class Player : Unit
 {
     private int[] maxExp = { 50, 100, 200, 300, 400, 500, 600, 700 }; // 레벨별 필요 경험치
     public Player()
@@ -33,15 +36,8 @@
     {
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine($"이름 : {Name}");
-            Console.WriteLine($"직업 : {Job}");
-            Console.WriteLine($"레벨 : {level + 1}    경험치 :{exp}/{maxExp[level]}");
-            Console.WriteLine($"체력 : {Hp} / {maxHp}");
-            Console.WriteLine($"마나 : {mp} / {maxMp}");
-            Console.WriteLine($"공격력 : {Atk}");
-            Console.WriteLine($"방어력 : {Def}");
-            Console.WriteLine($"금화 : {Gold}");
+            RenderStatusUI();
+
             Console.WriteLine("\n1. 나가기");
             int inputKey = GameManager.GM.SelectOption(1, false);
             switch (inputKey)
@@ -53,7 +49,58 @@
             }
         }
     }
-    
+
+    void RenderStatusUI()
+    {
+        StringBuilder[] tmi = JobTMI($"{Job}");
+        Console.Clear();
+        Console.SetCursorPosition(0, 0);
+        Console.Write("*-----------------------------------------------------------------------------------*");
+        Console.SetCursorPosition(0, 11);
+        Console.Write("*-----------------------------------------------------------------------------------*");
+        for (int i = 1; i < 11; i++)
+        {
+            Console.SetCursorPosition(0, i);
+            Console.Write("|");
+            Console.SetCursorPosition(84, i);
+            Console.Write("|");
+        }
+        Console.SetCursorPosition(3, 7);
+        GameManager.GM.DrawText(3, 2, "이름 ","blue"); Console.Write($": {Name}");
+        GameManager.GM.DrawText(3, 3, "직업 ","blue"); Console.Write($": {job}");
+        GameManager.GM.DrawText(60, 2, $"이력서","green");
+        GameManager.GM.DrawText(60, 3, $"Lv : {level+1}","white");
+        GameManager.GM.DrawText(68, 3, $"({exp}".PadLeft(4,' ')+" / " + $"{maxExp[Level+1]})".PadRight(4,' '),"white");
+        GameManager.GM.DrawText(60, 4, $"체력   : {Hp} / {maxHp}","white");
+        GameManager.GM.DrawText(60, 5, $"마나   : {Mp} / {MaxMp}", "white");
+        GameManager.GM.DrawText(60, 6, $"공격력 : {Atk}", "white");
+        GameManager.GM.DrawText(60, 7, $"방어력 : {Def}", "white");
+        GameManager.GM.DrawText(60, 8, $"골드   : {Gold}", "white");
+
+        GameManager.GM.DrawText(1, 5, "---------------------------------------------*", "white");
+        GameManager.GM.DrawText(46, 6, "|", "white");
+        GameManager.GM.DrawText(46, 7, "|", "white");
+        GameManager.GM.DrawText(46, 8, "|", "white");
+        GameManager.GM.DrawText(46, 9, "|", "white");
+        GameManager.GM.DrawText(46, 10, "|", "white");
+
+        if (Name.Length == 0)
+        {
+            GameManager.GM.DrawText(2, 6, $"소 개", "green");
+        }
+        else
+        {
+            GameManager.GM.DrawText(2, 6, $"{Name[0]}씨 :", "green");
+
+        }
+
+        GameManager.GM.DrawText(2, 7, $"{tmi[0]}", "white");
+        GameManager.GM.DrawText(2, 8, $"{tmi[1]}", "white");
+        GameManager.GM.DrawText(2, 9, $"{tmi[2]}", "white");
+
+        Console.SetCursorPosition(2, 12);
+    }
+
     public void CheckLevelup()
     {
         for (int i = level; i < maxExp.Length; i++) // maxExp 배열 사용
@@ -76,6 +123,44 @@
                 break;
             }
         }
+    }
+
+    StringBuilder[] JobTMI(string job)
+    {
+        StringBuilder[] tmi = new StringBuilder[3];
+
+        tmi[0] = new StringBuilder();
+        tmi[1] = new StringBuilder();
+        tmi[2] = new StringBuilder();
+
+        switch (job)
+        {
+            case "횟집 사장":
+                tmi[0].Append("아따~ 오늘 매물들 상태가 왜 이런겨~");
+                tmi[1].Append("김선장 배 띄우쇼");
+                tmi[2].Append("직접 확 다 잡아 가야쓰것소..");
+                break;
+            case "전문 낚시꾼":
+                tmi[0].Append("파고 퍼-펙트. 수온 퍼-펙트.");
+                tmi[1].Append("바람 퍼-펙트.");
+                tmi[2].Append("선장 출항.");
+                break;
+            case "해양 경찰":
+                tmi[0].Append("출항하기 전에 점검 나왔습니다.");
+                tmi[1].Append("신분증 제시하시고 구명 조끼 하세요.");
+                tmi[2].Append("술 드시지 마시고 이제 출항하세요.");
+                break;
+            case "참치그룹 회장":
+                tmi[0].Append("가족 건강과 행복, 아이들의 밝은 미래를 위해");
+                tmi[1].Append("신뢰와 만족을 최우선으로 만들어 가겠습니다.");
+                tmi[2].Append("                            ㅇㅇ산업 CEO 曰");
+                break;
+            default:
+
+                break;
+        }
+
+        return tmi;
     }
 
 }
